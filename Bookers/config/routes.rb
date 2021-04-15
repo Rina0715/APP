fdc8b5c593573d4a947adeeefe0cf7026b1899e7
Rina0715 <rk19910715@gmail.com>
2021-04-15 10:05:48 +0000

    [Fnish]課題

---
+++ Bookers/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'top' => 'homes#top'
+  get 'books' => 'books#index', as: 'books'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+end
+
